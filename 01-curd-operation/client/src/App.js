6d04920f918c3c0c05194899a585bc10f0ef68ab
bhaveshbalendra <balendrabhavesh@gmail.com>
2024-07-16 22:06:01 +0530

    new

--- 01-curd-operation/client/src/App.js
+++ 01-curd-operation/client/src/App.js
@@ -2,7 +2,39 @@ import "./App.css";
 // import { createBrowserRouter } from "react-router-dom";
 function App() {
   // const route = createBrowserRouter([{ path: "/" }]);
-  return <h1>hello world</h1>;
+  return (
+    <div className="container">
+      <div className="hero">
+        <button>Add User</button>
+        <table>
+          <tr>
+            <th>S.No</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Bhavesh Balendra</td>
+            <td>bhavesh@gmail.com</td>
+            <td>Delete Edit</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Bhavesh Balendra</td>
+            <td>bhavesh@gmail.com</td>
+            <td>Delete Edit</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Bhavesh Balendra</td>
+            <td>bhavesh@gmail.com</td>
+            <td>Delete Edit</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- 03-ecommerce/client/src/components/Card.jsx
+++ 03-ecommerce/client/src/components/Card.jsx
@@ -1,7 +1,5 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 
-/* eslint-disable react/prop-types */
 import { IoCart } from "react-icons/io5";
 import { FaBoltLightning } from "react-icons/fa6";
 import styles from "./Card.module.css";
@@ -12,7 +10,6 @@ import { useContext } from "react";
 import axios from "axios";
 
 function Cards({ item }) {
-  const [selectCard, setSelectCard] = useState({});
   const [quantity, setQuantity] = useState(1);
   const { userData } = useContext(MyContext);
 
@@ -25,7 +22,6 @@ function Cards({ item }) {
       });
       if (res.status === 200) {
         setQuantity(1);
-        setSelectCard({});
       }
     } catch (error) {
       if (error.response && error.response.status === 400) {
@@ -79,82 +75,3 @@ function Cards({ item }) {
 }
 
 export default Cards;
-
-// import { IoCart } from "react-icons/io5";
-// import { FaBoltLightning } from "react-icons/fa6";
-// import styles from "./Card.module.css";
-// import Buttons from "./Buttons";
-// import { useState } from "react";
-// import { MyContext } from "../App";
-// import { useContext } from "react";
-// import axios from "axios";
-
-// function Cards({ item }) {
-//   const [selectCard, setSelectCard] = useState({});
-//   const [quantity, setQuantity] = useState(1);
-//   const { userData } = useContext(MyContext);
-
-//   async function handleAddToCart(item) {
-//     setSelectCard(item);
-//     try {
-//       const res = await axios.post("http://localhost:5000/cart", {
-//         selectCard,
-//         userData,
-//         quantity,
-//       });
-//       if (res.status === 200) {
-//         setQuantity(1);
-//         selectCard({});
-//       }
-//     } catch (error) {
-//       if (error.res && error.res.status === 400) {
-//         console.log(error);
-//       } else {
-//         console.log("error unexpected");
-//       }
-//     }
-//   }
-
-//   return (
-//     <section>
-//       <div className={`${styles.section_card}`}>
-//         <div className="section-image">
-//           <img src={item.image} alt="" />
-//         </div>
-//         <div className={`${styles.section_content}`}>
-//           <p>{item.title}</p>
-//           <span>Price : ${item.price}</span>
-//           <div className={`${styles.section_quantity}`}>
-//             <Buttons
-//               onClick={() =>
-//                 setQuantity((quantity) => (quantity > 1 ? quantity - 1 : 1))
-//               }
-//             >
-//               -
-//             </Buttons>
-//             <span style={{ margin: "0 1rem" }}>{quantity}</span>
-//             <Buttons onClick={() => setQuantity((quantity) => quantity + 1)}>
-//               +
-//             </Buttons>
-//           </div>
-//           <div className={`${styles.section_actions}`}>
-//             <Buttons onClick={handleAddToCart}>
-//               <div className={styles.button_action}>
-//                 <IoCart />
-//                 <span> ADD TO CART</span>
-//               </div>
-//             </Buttons>
-//             <Buttons>
-//               <div className={styles.button_action}>
-//                 <FaBoltLightning />
-//                 <span> BUY NOW</span>
-//               </div>
-//             </Buttons>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-
-// export default Cards;
